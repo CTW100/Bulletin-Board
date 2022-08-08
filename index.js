563cea349534d4b48da0444bbc730a98800a36d8
CTW100 <sulsy887@naver.com>
2022-08-08 10:59:44 +0000

    Development Setting

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const methodOverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017');
+const db = mongoose.connection;
+
+db.once('open', () => {
+	console.log('DB connected!');
+});
+
+db.on('error', (err) => {
+	console.log('DB ERROR : ', err);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.use('/', require('./routes/home'));
+
+const port = 3000;
+app.listen(3000, (req, res) => {
+	console.log('Let us start');
+});
